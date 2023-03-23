0865ca0c52e7599c3024ece9979538fb4b3ffffa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 16:18:03 +0300

    Created the Container styled component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
+
 
 const Hero = () => {
   return (
