fa6f6a0e5456f7860e42f6f095ac5e1c7450750f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 16:14:06 +0300

    Created the Container styled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import styled from 'styled-components';
 import './App.css';
 import Contacts from './components/Contacts';
 import Hero from './components/Hero';
 import Who from './components/Who';
 import Works from './components/Works';
+
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
 
 function App()
 {
