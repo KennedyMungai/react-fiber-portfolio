3f6cf0ca324e58a555aafbf6862f1040e031481c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 16:21:32 +0300

    The refactoring is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
 const Section = styled.div`
   height: 100vh;
   background-color: rebeccapurple;
 `
-
 
 const Hero = () => {
   return (
