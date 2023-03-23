e95abc034ef6ee9035d25991b93308cf620d9957
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 16:10:34 +0300

    Added all the four components to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Contacts from './components/Contacts';
+import Hero from './components/Hero';
+import Who from './components/Who';
+import Works from './components/Works';
 
 function App()
 {
   return (
     <div className="App">
-
+      <Hero />
+      <Who />
+      <Works />
+      <Contacts />
     </div>
   );
 }
