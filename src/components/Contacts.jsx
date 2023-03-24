82e3f7b04bdd91809b36dbb0ae19d0503d25640c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 09:29:16 +0300

    Removed the background colors of the components

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 const Section = styled.div`
   height: 100vh;
-  background-color: #01b410;
   scroll-snap-align: center;
 `
 
--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 const Section = styled.div`
   height: 100vh;
-  background-color: #0d53ad;
   scroll-snap-align: center;
 `
 
